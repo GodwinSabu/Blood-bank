db3aedb9ed407082907d344f33dce022e4fcebe0
godwin sabu <godu1539@gmail.com>
2024-06-25 23:31:14 +0530

    blood-bank

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button} from 'antd';
+  
+
+function App() {
+  return (
+    <div>
+    <h1> Blood Bank  </h1>
+    <Button type="primary">Primary Button</Button>
+    </div>
+  );
+}
+
+export default App;
